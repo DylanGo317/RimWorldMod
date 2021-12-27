a46ee50f57483f073aa361034967f54f8483f39d
Dylan <dylanpwgo@gmail.com>
2021-12-26 19:25:44 -0800

    Added incendiary projectile its mod extension and put it in the mosin nagant 9130

---
+++ Source/TotalWar/TotalWar/ModExtension_IncendiaryBullet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+
+//Codes for a potential field that can be added to a def to inflict burn damage
+namespace TotalWar
+{
+    public class ModExtension_IncendiaryBullet : DefModExtension
+    {
+        public float addHediffChance = 0.9f;
+        public HediffDef hediffToAdd;
+    }
+}
---
+++ Source/TotalWar/TotalWar/Projectile_IncendiaryBullet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace TotalWar
+{
+    //Codes for the chance of the hediff being added to the target upon a successful shot
+    class Projectile_IncendiaryBullet : Bullet
+    {
+        public ModExtension_IncendiaryBullet Props => base.def.GetModExtension<ModExtension_IncendiaryBullet>();
+
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            if (Props != null && hitThing != null && hitThing is Pawn hitPawn)
+            {
+                //Chance for the hediff to be applied to the pawn
+                float rand = Rand.Value;
+                if (rand <= Props.addHediffChance)
+                {
+                    Messages.Message("TotalWar_IncendiaryBullet_SuccessMessage".Translate(this.launcher.Label, hitPawn.Label), 
+                        MessageTypeDefOf.NeutralEvent);
+                    Hediff burnOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+                    //Generates an int for the severity of the burn
+                    Random rnd = new Random();
+                    int randomSeverity = rnd.Next(1, 4);
+                    if (burnOnPawn != null)
+                    {
+                        burnOnPawn.Severity += randomSeverity;
+                    }
+                    else
+                    {
+                        Hediff hediff = HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+                        hediff.Severity = randomSeverity;
+                        hitPawn.health.AddHediff(hediff);
+                    }
+                }
+                else
+                {
+                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "TotalWar_IncendiaryBullet_FailureMote".Translate(Props.addHediffChance), 12f);
+                }
+            }
+        }
+    }
+}
