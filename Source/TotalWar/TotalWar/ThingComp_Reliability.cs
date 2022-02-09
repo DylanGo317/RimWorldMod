9c2ec5a9a27d556c1ad71830381dc4eb969b1c08
Dylan <dylanpwgo@gmail.com>
2022-02-09 08:27:56 -0800

    Reliability changes with quality and now displays

--- Source/TotalWar/TotalWar/ThingComp_Reliability.cs
+++ Source/TotalWar/TotalWar/ThingComp_Reliability.cs
@@ -81,5 +81,55 @@ namespace TotalWar
                 canShoot = true;
             }
         }
+
+        //Adds entries into the description for the values of the new weapon settings
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats()
+        {
+            if (weaponSuccess == 0f)
+            {
+                CompQuality compQual = parent.TryGetComp<CompQuality>();
+                if (compQual != null)
+                {
+                    float temp = Props.weaponSuccessChance;
+                    if (compQual.Quality == QualityCategory.Awful)
+                    {
+                        weaponSuccess = temp - ((1f - temp) * 2f);
+                    }
+                    else if (compQual.Quality == QualityCategory.Poor)
+                    {
+                        weaponSuccess = temp - ((1f - temp));
+                    }
+                    else if (compQual.Quality == QualityCategory.Normal)
+                    {
+                        weaponSuccess = temp;
+                    }
+                    else if (compQual.Quality == QualityCategory.Good)
+                    {
+                        weaponSuccess = temp + ((1f - temp) * 0.2f);
+                    }
+                    else if (compQual.Quality == QualityCategory.Excellent)
+                    {
+                        weaponSuccess = temp + ((1f - temp) * 0.4f);
+                    }
+                    else if (compQual.Quality == QualityCategory.Masterwork)
+                    {
+                        weaponSuccess = temp + ((1f - temp) * 0.6f);
+                    }
+                    else if (compQual.Quality == QualityCategory.Legendary)
+                    {
+                        weaponSuccess = temp + ((1f - temp) * 0.8f);
+                    }
+                }
+            }
+            IEnumerable<StatDrawEntry> enumerable = base.SpecialDisplayStats();
+            if (enumerable != null) { 
+                foreach (StatDrawEntry statDrawEntry in enumerable)
+                {
+                    yield return statDrawEntry;
+                }
+            }
+            yield return new StatDrawEntry(StatCategoryDefOf.Weapon_Ranged, "Time to fix weapon failure", weaponFailureFixTime.ToString() + " s", null, 9999, null, null, false);
+            yield return new StatDrawEntry(StatCategoryDefOf.Weapon_Ranged, "Base Weapon reliability", (weaponSuccess * 100).ToString() + "%", null, 9998, null, null, false);
+        }
     }
 }
--- Source/TotalWar/TotalWar/ThingComp_ReliabilityProperties.cs
+++ Source/TotalWar/TotalWar/ThingComp_ReliabilityProperties.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
-using RimWorld;
 
 namespace TotalWar
 {
@@ -16,17 +15,6 @@ namespace TotalWar
         public ThingComp_ReliabilityProperties()
         {
             this.compClass = typeof(ThingComp_Reliability);
-        }
-
-        //Adds entries into the description for the values of the new weapon settings
-        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(StatRequest req)
-        {
-            foreach (StatDrawEntry statDrawEntry in base.SpecialDisplayStats(req))
-            {
-                yield return statDrawEntry;
-            }
-            yield return new StatDrawEntry(StatCategoryDefOf.Weapon_Ranged, "Time to fix weapon failure", weaponFailureFixTime.ToString() + " s", null, 9999, null, null, false);
-            yield return new StatDrawEntry(StatCategoryDefOf.Weapon_Ranged, "Base Weapon reliability", (weaponSuccessChance * 100).ToString() + "%", null, 9998, null, null, false);
         }
     }
 }
